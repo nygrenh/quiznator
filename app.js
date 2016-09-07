ac721dc3a722703b0e3065d22938168b5096d6fc
Kalle O Ilves <kalleilv@mba-13.cs.helsinki.fi>
2016-09-07 09:40:38 +0300

    added readme

--- app.js
+++ app.js
@@ -15,14 +15,11 @@ var server = require('./server');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
