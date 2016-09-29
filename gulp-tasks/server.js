3b9f829656be0a22f1fdf3a8843ffa76d9961f29
Kalle O Ilves <kalleilv@mba-13.cs.helsinki.fi>
2016-09-29 11:34:06 +0300

    changed development client secret

--- gulp-tasks/server.js
+++ gulp-tasks/server.js
@@ -5,10 +5,6 @@ const constants = require('./constants');
 
 let quiznatorClient = {};
 
-if(process.env.NODE_ENV === 'development') {
-  quiznatorClient = require('quiznator-client.config.js');
-}
-
 module.exports = () => {
   nodemon({
     script: './bin/www',
@@ -20,8 +16,8 @@ module.exports = () => {
       PLUGIN_SCRIPT_URL: 'http://localhost:3000/javascripts/plugin.min.js',
       PLUGIN_STYLE_URL: 'http://localhost:3000/stylesheets/plugin.min.css',
       TMC_URL: 'https://tmc.mooc.fi',
-      QUIZNATOR_CLIENT_ID: quiznatorClient.CLIENT_ID,
-      QUIZNATOR_CLIENT_SECRET: quiznatorClient.CLIENT_SECRET
+      QUIZNATOR_CLIENT_ID: 'quiznatorApp',
+      QUIZNATOR_CLIENT_SECRET: 'quiznatorSecret'
     }
   });
 };
