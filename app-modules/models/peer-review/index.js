fee033d62444e4e2986f52704a7681a0c57f11fd
Henrik Nygren <nygrenh@gmail.com>
2017-10-31 05:54:57 +0200

    Add grading to peer review schema

--- app-modules/models/peer-review/index.js
+++ app-modules/models/peer-review/index.js
@@ -7,7 +7,8 @@ const schema = new mongoose.Schema({
   giverAnswererId: { type: String, required: true },
   review: { type: String, maxlength: 10000, minlength: 1, required: true },
   rejectedQuizAnswerId: { type: mongoose.Schema.Types.ObjectId, ref: 'QuizAnswer', required: true },
-  chosenQuizAnswerId: { type: mongoose.Schema.Types.ObjectId, ref: 'QuizAnswer', required: true }
+  chosenQuizAnswerId: { type: mongoose.Schema.Types.ObjectId, ref: 'QuizAnswer', required: true },
+  grading: { type: mongoose.Schema.Types.Mixed, required: false },
 }, { timestamps: true });
 
 require('./methods')(schema);
