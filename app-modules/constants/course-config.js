3db40995c75a47ad1fe4b088b8976e601517e8e7
Mikko Pyykkö <mikko.pyykko@helsinki.fi>
2018-06-27 21:43:14 +0300

    elements-of-ai: decreased minimum peer reviews received to 2

--- app-modules/constants/course-config.js
+++ app-modules/constants/course-config.js
@@ -2,7 +2,7 @@ const config = {
   API_URI: process.env.API_URL, // 'http://127.0.0.1:3000', // 'http://quiznator.herokuapp.com', // process.env.API_URI
   DB_URI: process.env.MONGO_URI, //'mongodb://127.0.0.1:27017/test'
   MINIMUM_PEER_REVIEWS_GIVEN: 3,
-  MINIMUM_PEER_REVIEWS_RECEIVED: 3,
+  MINIMUM_PEER_REVIEWS_RECEIVED: 2,
   MINIMUM_SPAM_FLAGS_TO_FAIL: 3,
   MAXIMUM_SPAM_FLAGS_TO_PASS: 0,
   MINIMUM_PROGRESS_TO_PASS: 90,
