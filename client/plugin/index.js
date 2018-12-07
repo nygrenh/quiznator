98f54df190bbd9a80445512949b88d707af1feb5
Henrik Nygren <nygrenh@gmail.com>
2018-12-07 12:51:48 +0200

    Add an option to retrigger quiz loading

--- client/plugin/index.js
+++ client/plugin/index.js
@@ -1,37 +1,46 @@
 try {
-  require('babel-polyfill');
-} catch(e) {}
+  require("babel-polyfill");
+} catch (e) {}
 
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-import Dashboard from 'components/dashboard';
-import DashboardOpener from 'components/dashboard/dashboard-opener';
-import QuizLoader from 'components/quiz-loader';
-import store from 'state/store';
-import withClassPrefix from 'utils/class-prefix';
-import syncQuiznatorWithStore from 'utils/sync-quiznator-with-store';
+import Dashboard from "components/dashboard";
+import DashboardOpener from "components/dashboard/dashboard-opener";
+import QuizLoader from "components/quiz-loader";
+import store from "state/store";
+import withClassPrefix from "utils/class-prefix";
+import syncQuiznatorWithStore from "utils/sync-quiznator-with-store";
 
 window.Quiznator = syncQuiznatorWithStore(store);
 
-Array.from(document.querySelectorAll(`.${withClassPrefix('plugin')}`))
-  .forEach(quiz => {
+window.loadQuizzes = function() {
+  Array.from(
+    document.querySelectorAll(`.${withClassPrefix("plugin")}`)
+  ).forEach(quiz => {
     render(
       <Provider store={store}>
-        <QuizLoader id={quiz.getAttribute('quiz-id') || quiz.getAttribute('data-quiz-id')}/>
+        <QuizLoader
+          id={quiz.getAttribute("quiz-id") || quiz.getAttribute("data-quiz-id")}
+        />
       </Provider>,
       quiz
     );
   });
+};
 
-const dashboardContainer = document.querySelector(`.${withClassPrefix('dashboard')}`);
+window.loadQuizzes()
 
-if(dashboardContainer) {
+const dashboardContainer = document.querySelector(
+  `.${withClassPrefix("dashboard")}`
+);
+
+if (dashboardContainer) {
   render(
     <Provider store={store}>
-      <Dashboard/>
+      <Dashboard />
     </Provider>,
     dashboardContainer
   );
